425d83e8f379f8b6dcebae89a1778dfbac59ed8d
Yuto299 <ota.se.ke@outlook.jp>
2025-07-13 19:06:24 +0900

    Step 3: finish

--- game.js
+++ game.js
@@ -1,10 +1,29 @@
 let gamePattern = [];
 let buttonColors = ["red", "blue", "green", "yellow"];
 
+function flashButton(randomChosenColor) {
+  $("#" + randomChosenColor)
+    .fadeOut(100)
+    .fadeIn(100);
+  playSound(randomChosenColor);
+}
+
 function nextSequence() {
   let randomNumber = Math.floor(Math.random() * 4);
   let randomChosenColor = buttonColors[randomNumber];
   gamePattern.push(randomChosenColor);
+
+  flashButton(randomChosenColor);
+}
+
+$(".btn").click(function () {
+  let userChosenColor = $(this).attr("id");
+  flashButton(userChosenColor);
+});
+
+function playSound(color) {
+  let audio = new Audio("sounds/" + color + ".mp3");
+  audio.play();
 }
 
 nextSequence();
