94524b3751d26a641e12fd3c568d701ab0a77032
Yuto299 <ota.se.ke@outlook.jp>
2025-07-13 18:42:16 +0900

    Step 2: finish

---
+++ game.js
@@ -0,0 +1,10 @@
+let gamePattern = [];
+let buttonColors = ["red", "blue", "green", "yellow"];
+
+function nextSequence() {
+  let randomNumber = Math.floor(Math.random() * 4);
+  let randomChosenColor = buttonColors[randomNumber];
+  gamePattern.push(randomChosenColor);
+}
+
+nextSequence();
